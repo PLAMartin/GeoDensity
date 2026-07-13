0306aa13d32cac83b235ea63919d301d70ab2c9b
Phil Martin <incyweb@gmail.com>
2026-07-13 16:06:59 +0100

    Add Google Analytics
    Loads gtag.js behind NEXT_PUBLIC_GA_MEASUREMENT_ID, following the same
    env-gated pattern already used for Plausible.

--- components/AnalyticsProvider.tsx
+++ components/AnalyticsProvider.tsx
@@ -6,21 +6,38 @@ import { trackPageView } from "@/lib/analytics";
 
 export default function AnalyticsProvider() {
   const plausibleDomain = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN;
+  const gaMeasurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
   useEffect(() => {
     trackPageView();
   }, []);
 
-  if (!plausibleDomain) {
-    return null;
-  }
-
   return (
-    <Script
-      defer
-      data-domain={plausibleDomain}
-      src="https://plausible.io/js/script.js"
-      strategy="afterInteractive"
-    />
+    <>
+      {plausibleDomain ? (
+        <Script
+          defer
+          data-domain={plausibleDomain}
+          src="https://plausible.io/js/script.js"
+          strategy="afterInteractive"
+        />
+      ) : null}
+      {gaMeasurementId ? (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
+            strategy="afterInteractive"
+          />
+          <Script id="ga-gtag-init" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gaMeasurementId}');
+            `}
+          </Script>
+        </>
+      ) : null}
+    </>
   );
 }
